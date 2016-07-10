ea4e7fe8051b47d3db314edfbe68b5754f0aadd2
Joe <joe@appstretto.com>
2016-07-10 13:29:41 +0200

    add auth0 provider

--- app/app.ts
+++ app/app.ts
@@ -1,14 +1,29 @@
 import {Component} from '@angular/core';
 import {Platform, ionicBootstrap} from 'ionic-angular';
 import {StatusBar} from 'ionic-native';
+import {provide} from '@angular/core';
+import {Http} from '@angular/http';
+import {AuthHttp, AuthConfig} from 'angular2-jwt';
+import {AuthService} from './providers/auth/auth';
+import {Type} from '@angular/core';
 import {CardListPage} from './pages/card-list/card-list';
+import {ProfilePage} from './pages/profile/profile';
 
 
 @Component({
-  template: '<ion-nav [root]="rootPage"></ion-nav>'
+  template: '<ion-nav [root]="rootPage"></ion-nav>',
+  providers: [
+    provide(AuthHttp, {
+      useFactory: (http) => {
+        return new AuthHttp(new AuthConfig(), http);
+      },
+      deps: [Http]
+    }),
+    AuthService
+  ]
 })
 export class MyApp {
-  rootPage: any = CardListPage;
+  rootPage: any = ProfilePage;
 
   constructor(platform: Platform) {
     platform.ready().then(() => {
---
+++ app/providers/auth/auth.ts
@@ -0,0 +1,56 @@
+import {Storage, LocalStorage} from 'ionic-angular';
+import {AuthHttp, JwtHelper, tokenNotExpired} from 'angular2-jwt';
+import {Injectable} from '@angular/core';
+import {Observable} from 'rxjs/Rx';
+
+// Avoid name not found warnings
+declare var Auth0Lock: any;
+
+@Injectable()
+export class AuthService {
+  jwtHelper: JwtHelper = new JwtHelper();
+  lock = new Auth0Lock('59QBWgUOcZnVfInoZhAo0kFgF3epVlSM', 'joewoodson.eu.auth0.com');
+  local: Storage = new Storage(LocalStorage);
+  user: Object;
+
+  constructor(private authHttp: AuthHttp) {
+    // If there is a profile saved in local storage
+    this.local.get('profile').then(profile => {
+      this.user = JSON.parse(profile);
+    }).catch(error => {
+      console.log(error);
+    });
+  }
+
+  public authenticated() {
+    // Check if there's an unexpired JWT
+    return tokenNotExpired();
+  }
+
+  public login() {
+    // Show the Auth0 Lock widget
+    this.lock.show({
+      authParams: {
+        scope: 'openid offline_access',
+        device: 'Mobile device'
+      }
+    }, (err, profile, token, accessToken, state, refreshToken) => {
+      if (err) {
+        alert(err);
+      }
+      // If authentication is successful, save the items
+      // in local storage
+      this.local.set('profile', JSON.stringify(profile));
+      this.local.set('id_token', token);
+      this.local.set('refresh_token', refreshToken);
+      this.user = profile;
+    });    
+  }
+
+  public logout() {
+    this.local.remove('profile');
+    this.local.remove('id_token');
+    this.local.remove('refresh_token');
+    this.user = null;
+  }
+}
